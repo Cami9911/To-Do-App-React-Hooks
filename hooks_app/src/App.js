9a5ba0d24602fb4bdc07ff8159c55c2ed7864197
Cami9911 <camelia.tirnovan@yahoo.com>
2020-06-09 21:34:43 +0300

    almost done

--- hooks_app/src/App.js
+++ hooks_app/src/App.js
@@ -1,7 +1,37 @@
 import React, {useState} from 'react'
+import './App.css'
 
-function Todo({todo,index}){
-  return <div className="todo">{todo.text}</div>
+function Todo({ todo, index, completeTodo, removeTodo, editTodo }) {
+  return <div style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+    className="todo">
+    {todo.text}
+    <div>
+      <button onClick={() => completeTodo(index)}>Complete</button>
+      <button onClick={() => removeTodo(index)}>x</button>
+      <button onClick={() => editTodo(index)}>Edit</button>
+    </div>
+  </div>
+}
+
+function TodoForm({addTodo}){
+  const [value, setValue]=useState('');
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+      className="input" 
+      value={value} 
+      placeholder="Add Task.."
+      onChange={e =>setValue(e.target.value)}/>
+    </form>
+  )
 }
 
 function App(){
@@ -20,11 +50,41 @@ function App(){
     }
   ])
 
+  const addTodo = text =>{
+    const newTodos = [...todos, {text}];
+    setTodos(newTodos)
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
+  const editTodo = (text,index) => {
+    const newTodos = [...todos];
+    newTodos[index].text={text};
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos);
+  }
+
   return(
     <div className="app">
       <div className="todo-list">
+      <TodoForm addTodo={addTodo} />
         {todos.map((todo,index)=>(
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+          key={index}
+          index={index}
+          todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo} 
+          editTodo={editTodo}/>
         ))}
       </div>
     </div>
